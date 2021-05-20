00680966b098a8e31e7b01c7ad9babdc045b91c2
Duke Takle (vesta) <duke.takle@corteva.com>
2021-05-20 14:50:41 -0500

    decruft

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 import json
 
 import matplotlib.pyplot as plt
-import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -27,14 +26,10 @@ def read_metrics(filename):
 
 def draw_something(xstuff, ystuff):
     logger.info('draw_something() called')
-    xpoints = np.array(xstuff)
-    ypoints = np.array(ystuff)
-
     plt.plot(xstuff, ystuff)
     plt.xticks(xstuff, xstuff, rotation=-90)
-    # plt.tight_layout(rect=(0.1, 0.1, 1, 1))
-    plt.tight_layout(pad=2.25)  # , w_pad=1.5, h_pad=0)
-    plt.title("SMB v3 Connections (average / hour) 2")
+    plt.tight_layout(pad=2.25)
+    plt.title("SMB v3 Connections (average / hour)")
     plt.xlabel("Time")
     plt.ylabel("Connections")
     plt.savefig('food.png')
